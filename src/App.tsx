7f7395ff8f15eb2d7d6d0d78bdaa5a26f5470e11
agkz1st <ahmed.agk22@gmail.com>
2022-04-22 11:33:35 +0200

    - Added   -	new file:   .env   -	new file:   supabaseClient.js - Created a react query client in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { PageLayout } from 'components/layout';
 import MonthSummaryPage from 'pages/MonthSummaryPage/MonthSummaryPage';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/month-summary" />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/month-summary" />} />
 
-        <Route path="/" element={<PageLayout />}>
-          <Route path="month-summary" element={<MonthSummaryPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/" element={<PageLayout />}>
+            <Route path="month-summary" element={<MonthSummaryPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 

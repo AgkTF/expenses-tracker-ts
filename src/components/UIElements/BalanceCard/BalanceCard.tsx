823184c6286f139b211e277422359c0a7d6ece5d
agkz1st <ahmed.agk22@gmail.com>
2022-04-29 11:39:43 +0200

    - Created a zustand	store:   src/store/useStore.ts just for testing it by storing some default app state in it. - Probably this state will be moved to supabase as well.

--- src/components/UIElements/BalanceCard/BalanceCard.tsx
+++ src/components/UIElements/BalanceCard/BalanceCard.tsx
@@ -1,4 +1,5 @@
 import { moneyFormatter } from 'utils/helpers/numbers.helpers';
+import { useStore } from 'store/useStore';
 
 type Props = {
   availableBalance: number;
@@ -7,6 +8,8 @@ type Props = {
 };
 
 const BalanceCard = ({ availableBalance, budget, totalSpent }: Props) => {
+  const defaultCurrency = useStore(state => state.currency);
+
   return (
     <div className="p-4 w-72 bg-gray-100 rounded-md shadow-air77">
       <p className="text-slate-400 font-medium text-sm tracking-wider">
@@ -14,20 +17,22 @@ const BalanceCard = ({ availableBalance, budget, totalSpent }: Props) => {
       </p>
 
       <p className="mt-1 text-slate-600 text-xl font-bold">
-        {moneyFormatter(availableBalance, 'USD')}
+        {moneyFormatter(availableBalance, defaultCurrency)}
       </p>
 
       <div className="mt-6 flex items-center justify-between text-sm text-slate-400">
         <p>
           <span className="font-light">Spent </span>
           <span className="font-semibold">
-            {moneyFormatter(totalSpent, 'USD')}
+            {moneyFormatter(totalSpent, defaultCurrency)}
           </span>
         </p>
 
         <p>
           <span className="font-light">Budget </span>
-          <span className="font-semibold">{moneyFormatter(budget, 'USD')}</span>
+          <span className="font-semibold">
+            {moneyFormatter(budget, defaultCurrency)}
+          </span>
         </p>
       </div>
     </div>
